96b8680fe4b7507aa376ff8e541c4171da2b3060
hexuntao <hexuntao@foxmail.com>
2025-11-14 11:05:55 +0800

    test

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+const StyledCard = styled.div`
+  background-color: red;
+  box-shadow: inset 0 0 0 1px hsla(0, 0%, 100%, 0.05);
+`;
+
+export default StyledCard;
